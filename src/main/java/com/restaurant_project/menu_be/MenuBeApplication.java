71b71c286c845dc24a37d5ad809b6aa0ea4501df
ginguyenn <ginguyenn@gmail.com>
2023-06-16 00:56:44 +0200

    first commit

---
+++ src/main/java/com/restaurant_project/menu_be/MenuBeApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant_project.menu_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurant_project/menu_be/MenuBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant_project.menu_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
